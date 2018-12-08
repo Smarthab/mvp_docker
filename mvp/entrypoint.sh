5d17f3a01324dab09b40766fd4ee860440152552
dirtech@smarthab.tech <ilias.gar@gmail.com>
2018-12-08 16:48:45 +0100

    updated entrypoint script

--- mvp/entrypoint.sh
+++ mvp/entrypoint.sh
@@ -6,17 +6,17 @@ cd $HOME/mvp
 
 echo "Deploying logger contract"
 
-dune exec ./deploy.exe -- --account=$1 --secret=$HOME/writer_secret --solidity=$HOME/mvp/logger.sol --uri=$2 &
+LOGGER=`dune exec ./deploy.exe -- --account=$1 --secret=$HOME/writer_secret --solidity=$HOME/mvp/logger.sol --uri=$2`
 
 dune exec ./producer.exe -- --addr="tcp://127.0.0.1:5555" --secret=$HOME/writer_secret &
 
-# dune exec ./processor.exe --\
-#  --account=$1\
-#  --secret=$HOME/writer_secret\
-#  --listen="tcp://127.0.0.1:5555"\
-#  --contract="TODO"\
-#  --dbfile="packets"\
-#  --solidity="logger.sol"\
-#  --uri=$2&
+dune exec ./processor.exe --\
+ --account=$1\
+ --secret=$HOME/writer_secret\
+ --listen="tcp://127.0.0.1:5555"\
+ --contract="${LOGGER}"\
+ --dbfile="packets"\
+ --solidity="logger.sol"\
+ --uri=$2&
 
 
